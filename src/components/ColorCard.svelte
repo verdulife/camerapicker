730dd686893709f11f0960e6bb799d0e000b0900
verdulife <verdu@live.com>
2025-04-01 16:18:02 +0200

    fix colo naming with input

--- src/components/ColorCard.svelte
+++ src/components/ColorCard.svelte
@@ -12,13 +12,13 @@
   id={`hc_${id}`}
   class="flex w-full flex-col rounded-xl border border-neutral-800/10 bg-white p-2 shadow gap-1"
 >
-  <a href="/colors/{r}-{g}-{b}" aria-label={name}>
+  <a href="/colors/{r}-{g}-{b}" aria-label={name} class="flex w-full flex-col gap-2">
     <figure
       style={`background-color: ${rgbOut}`}
       class="aspect-square w-full rounded-md border border-neutral-800/10"
     ></figure>
 
-    <footer class="flex w-full flex-col text-left text-neutral-800 py-2">
+    <footer class="flex w-full flex-col text-left text-neutral-800">
       <h2 class="font-bold">{name}</h2>
     </footer>
   </a>
