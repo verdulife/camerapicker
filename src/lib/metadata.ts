18e3a327efe6f5ea12af70d03d4d4f9296f37676
verdulive <verdu@live.com>
2025-04-02 13:37:42 +0200

    meta info in spanish

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "HunterColor™ | The camera color picker",
-  description: "Hunt color codes from your camera and share them to the world",
+  title: "HunterColor™ | Captura, guarda y comparte colores",
+  description: "Captura colores con tu teléfono o tableta, guarda tus favoritos y compártelos. Obtén fácilmente los códigos RGB, Hex, CMYK, Pantone, RAL, Lab o HSL de cualquier color.",
   image: "card.jpg",
   name: "HunterColor™",
 }
