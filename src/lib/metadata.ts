93c7bfebc5fb2598c2eff3a5221c8912645c34de
verdulive <verdu@live.com>
2025-04-09 17:08:19 +0200

    update og

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "HunterColor™ | Captura, guarda y comparte colores",
   description: "Captura colores con tu teléfono o tableta, guarda tus favoritos y compártelos. Obtén fácilmente los códigos RGB, Hex, CMYK, Pantone, RAL, Lab o HSL de cualquier color.",
-  image: "card.jpg",
+  image: "/card.jpg",
   name: "HunterColor™",
 }
